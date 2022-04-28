8cf82b1a1db858fb552b5be7352a52105f137e86
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2022-04-28 20:04:47 -0300

    ✅ Add first passing tests

--- src/textProcessorFluentAPI.js
+++ src/textProcessorFluentAPI.js
@@ -1,1 +1,33 @@
-// TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+// O objetivo do FLuent API e executar tarefas como um pipeline, step by step e no fim chama o build.
+// Muito similar ao pattern Builder, a diferenca é sobre o processo (chamando métodos).
+// enquanto o Builder é sobre construção de objetos.
+class TextProcessorFluentAPI {
+  #content
+  constructor(content) {
+    this.#content = content
+  }
+
+  extractCSVData() {
+    const lines = this.#content.split('\n')
+    const headers = lines.shift().split(';')
+    headers.pop() // Retirar o new line (\r)
+
+    const treatedData = lines.map(line => {
+      const values = line.split(';')
+      const data = {}
+
+      headers.forEach((header, index) => {
+        data[header] = values[index]
+      })
+      return data
+    })
+
+    this.#content = treatedData
+    return this
+  }
+
+  build() {
+    return this.#content
+  }
+}
+module.exports = TextProcessorFluentAPI
--- test/textProcessorFluentAPI.test.js
+++ test/textProcessorFluentAPI.test.js
@@ -1,1 +1,25 @@
 // TODO: Dê uma olhada no projeto oficial do módulo 06 (Expressões Regulares - RegExp) para implementar este arquivo.
+
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
+const TextProcessorFluentAPI = require('../src/textProcessorFluentAPI')
+const mock = require('./mock/valid')
+const mockExpected = require('./mock/validExpected')
+
+describe('TextProcessorAPI', () => {
+
+
+  it('#build', () => {
+    const result = new TextProcessorFluentAPI(mock).build()
+
+    expect(result).to.be.deep.equal(mock)
+  })
+  
+  it('#extractCSVData', () => {
+    const result = new TextProcessorFluentAPI(mock)
+      .extractCSVData()
+      .build()
+
+    expect(result).to.be.deep.equal(mockExpected)
+  })
+})
